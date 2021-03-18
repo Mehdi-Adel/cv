a966a340c60c98102576bef15eb178941e9d5d85
Mehdi-Adel <zeadel.mehdi@gmail.com>
2021-03-18 16:57:55 +0100

    first commit

---
+++ js/functions.js
@@ -0,0 +1,28 @@
+function loaded(callable){
+    document.addEventListener("DOMContentLoaded", callable);
+};
+
+function $(selector){
+    return document.querySelector(selector);
+};
+
+function $$(selector){
+    return document.querySelectorAll(selector);
+};
+
+function screenWidth(lar){
+    if(lar >= 576 && $("body > nav ul").classList.contains("active")){
+        $("body > nav ul").classList.toggle("active");
+    }
+};
+
+loaded(function(){
+    var lar = window.innerWidth;
+    window.addEventListener('resize', function(event){
+        lar = window.innerWidth;
+        screenWidth(lar);
+    });            $(".toggle-menu").addEventListener("click", function(){
+        $("body > nav ul").classList.toggle("active");
+    });
+    screenWidth(lar);
+});
